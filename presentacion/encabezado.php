e079509eaae52ffb2210c0ef3c4ef62a07026395
Jonathan Escobar <escobarjsa@gmail.com>
2020-07-15 02:15:33 -0500

    Subiendo el proyecto de veterianaria

---
+++ presentacion/encabezado.php
@@ -0,0 +1,12 @@
+<div class="col-12">
+    <section class="hero is-Warning ">
+        <div class="hero-body">
+            <div class="container ">
+                <h1 class="title text-center">
+                    Veterinaria un mejor vivir
+                </h1>
+            </div>
+        </div>
+    </section>
+
+</div>
